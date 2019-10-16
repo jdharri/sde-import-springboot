9b4b63f217fa95b73661de46008399a6de9e02cd
joel <joel@2017JHARRI4W4QB>
2019-10-16 14:14:14 -0400

    generating properties file

--- src/main/java/mil/army/dcgs/SDEIMport/Importer.java
+++ src/main/java/mil/army/dcgs/SDEIMport/Importer.java
@@ -1,7 +1,7 @@
-
 package mil.army.dcgs.SDEIMport;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,35 +9,119 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author jdhar
  */
+@Service
 public class Importer {
-     try {
+
+    @Autowired
+    private FolderConfigRepository repo;
+
+    ConcurrentLinkedQueue fileQueue;
+
+    public void folderWatcher() {
+
+        List<FolderConfig> configs = repo.findAll();
+
+        configs.forEach(c -> {
+            insertIntoSDE(c);
+        });
+    }
+
+    @Async
+    public void insertIntoSDE(FolderConfig c) {
+        try {
             WatchService watchService = FileSystems.getDefault().newWatchService();
-            Path path = Paths.get(writeFile("test"));
-            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
+
+            Path path = Paths.get(c.getDirectory());
+            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
             WatchKey key;
             while ((key = watchService.take()) != null) {
                 for (WatchEvent<?> event : key.pollEvents()) {
                     System.out.println(
                             "Event kind:" + event.kind()
                             + ". File affected: " + event.context() + ".");
-                    if (event.kind().equals(StandardWatchEventKinds.ENTRY_CREATE)) {
-                        System.out.println("new file created");
-                        callSDEImport("stuff");
-                        
+                    /**
+                     * sdeimport -o update_else_insert {-l <table,column> | -t
+                     * <table>} -K <key_columns> [-V <version_name>] -f
+                     * <{export_file | -}> [-q] [-v] [-c <commit_interval>] [-i
+                     * {<service> | <port#> | <direct connection>}] [-s
+                     * <server_name>] [-D <database_name>] -u <DB_user_name>
+                     * [-p <DB_user_password>]
+                     */
+                    fileQueue.add(event.context());
+
+                    List<String> commands = new ArrayList<>();
+                    commands.add("cmd.exe");
+                    commands.add("/c");
+                    commands.add("dir");
+                    commands.add("-o");
+                    commands.add("update_else_insert");
+                    commands.add("-t");
+                    commands.add(c.getTableName());
+                    commands.add("-f");
+                    commands.add(event.context().toString());
+                    commands.add("-s");
+                    commands.add(c.getSdeHost());
+                    commands.add("-D");
+                    commands.add(c.getSdeDatabase());
+                    commands.add("-u");
+                    commands.add(c.getSdeUsername());
+                    commands.add("-p");
+                    commands.add(c.getSdePassword());
+                    commands.add("-i");
+                    commands.add(c.getSdePort());
+                    if (c.getKeyFields().length() > 0) {
+                        commands.add("-K");
+                        commands.add(c.getKeyFields());
                     }
+                    ProcessBuilder pb = new ProcessBuilder(commands);
+                    final Process p = pb.start();
+                    final InputStream expout = p.getInputStream();
+                    final int exitcode = p.waitFor();
+                    assert exitcode == 0;
                 }
-                key.reset();
             }
-        } catch (IOException ex) {
-            Logger.getLogger(SDEImport.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(SDEImport.class.getName()).log(Level.SEVERE, null, ex);
+            key.reset();
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(Importer.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+    }
+//    public void insertIntoSDE(){
+//     try {
+//            WatchService watchService = FileSystems.getDefault().newWatchService();
+//            Path path = Paths.get(writeFile("test"));
+//            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
+//            WatchKey key;
+//            while ((key = watchService.take()) != null) {
+//                for (WatchEvent<?> event : key.pollEvents()) {
+//                    System.out.println(
+//                            "Event kind:" + event.kind()
+//                            + ". File affected: " + event.context() + ".");
+//                    if (event.kind().equals(StandardWatchEventKinds.ENTRY_CREATE)) {
+//                        System.out.println("new file created");
+//                        callSDEImport("stuff");
+//                        
+//                    }
+//                }
+//                key.reset();
+//            }
+//        } catch (IOException ex) {
+//            Logger.getLogger(SDEImport.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(SDEImport.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
 }
--- src/main/java/mil/army/dcgs/SDEIMport/MainView.java
+++ src/main/java/mil/army/dcgs/SDEIMport/MainView.java
@@ -1,16 +1,20 @@
 package mil.army.dcgs.SDEIMport;
 
+import com.vaadin.flow.component.Component;
 import org.springframework.util.StringUtils;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
@@ -19,57 +23,66 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 @Route
 public class MainView extends VerticalLayout {
-
     
     private final FolderConfigRepository repo;
-
+    
     private final FolderConfigEditor editor;
-
+    
     final Grid<FolderConfig> grid;
     final TextField filter;
-
+    
     private final Button addNewBtn;
-
+    private final Button sysConfigBtn;
+    
     public MainView(FolderConfigRepository repo, FolderConfigEditor folderEditor) {
-
+        
         this.repo = repo;
         this.editor = folderEditor;
         this.grid = new Grid<>(FolderConfig.class);
         this.filter = new TextField();
         this.addNewBtn = new Button("New Configuration", VaadinIcon.PLUS.create());
-
-        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+        this.sysConfigBtn = new Button("System Configuration", VaadinIcon.COG.create());
+        
+        Dialog dialog = new Dialog();
+        TextField sdeImportExePath = new TextField();
+        sdeImportExePath.setLabel("path to sdeimport.exe");
+        dialog.add(sdeImportExePath);
+        dialog.add(new Label("Close me with the esc-key or an outside click"));
+        dialog.setWidth("400px");
+        dialog.setHeight("150px");
+        sysConfigBtn.addClickListener(event -> dialog.open());
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn, sysConfigBtn);
         add(actions, grid, editor);
-
+        
         grid.setHeight("200px");
         grid.setColumns("id", "directory", "tableName", "sdeDatabase", "sdePassword");
         grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
-
+        
         filter.setPlaceholder("Filter by directory");
-
+        
         filter.setValueChangeMode(ValueChangeMode.EAGER);
         filter.addValueChangeListener(e -> listConfigs(e.getValue()));
-
+        
         grid.asSingleSelect().addValueChangeListener(e -> {
             editor.editConfig(e.getValue());
         });
 
-		// Instantiate and edit new Customer the new button is clicked
-		addNewBtn.addClickListener(e -> editor.editConfig(new FolderConfig("", "", "", "")));
+        // Instantiate and edit new Customer the new button is clicked
+        addNewBtn.addClickListener(e -> editor.editConfig(new FolderConfig("", "", "", "")));
 
-		// Listen changes made by the editor, refresh data from backend
-		editor.setChangeHandler(() -> {
-			editor.setVisible(false);
-			listConfigs(filter.getValue());
-		});
+        // Listen changes made by the editor, refresh data from backend
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            listConfigs(filter.getValue());
+        });
 
-		// Initialize listing
-		listConfigs(null);
+        // Initialize listing
+        listConfigs(null);
     }
-
+    
     void listConfigs(String filterText) {
         if (StringUtils.isEmpty(filterText)) {
-
+            
             grid.setItems(repo.findAll());
         } else {
             grid.setItems(repo.findByDirectoryStartsWithIgnoreCase(filterText));
